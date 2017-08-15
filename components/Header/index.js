08383f3a89a68096fc029e7b585e49c45ce0ec31
JakkritS <jakk.sutt@gmail.com>
2017-08-16 01:52:44 +0700

    add nprogress

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,8 +1,14 @@
+/* eslint-disable */
+import React from 'react';
 import PropTypes from 'prop-types';
+import NProgress from 'nprogress';
+import Head from 'next/head';
+import Router from 'next/router';
 import LinkList from '../../components/LinkList';
 import { Header as StyledHeader } from './styles';
 import connect from './store';
 
+/*
 const Header = ({ pathname, authenticated, actions: { logout } }) =>
   <StyledHeader>
     <LinkList
@@ -11,7 +17,34 @@ const Header = ({ pathname, authenticated, actions: { logout } }) =>
       logout={logout}
     />
   </StyledHeader>;
+*/
 
+class Header extends React.Component {
+  componentDidMount() {
+    Router.onRouteChangeStart = url => {
+      console.log(`Loading: ${url}`);
+      NProgress.start();
+    };
+    Router.onRouteChangeComplete = () => NProgress.done();
+    Router.onRouteChangeError = () => NProgress.done();
+  }
+
+  render() {
+    const { pathname, authenticated, actions: { logout } } = this.props;
+    return (
+      <StyledHeader>
+        <Head>
+          <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+        </Head>
+        <LinkList
+          pathname={pathname}
+          authenticated={authenticated}
+          logout={logout}
+        />
+      </StyledHeader>
+    );
+  }
+}
 Header.defaultProps = {
   authenticated: false
 };
